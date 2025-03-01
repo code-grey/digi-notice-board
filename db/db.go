eb1d8069773b7a89ef06a7fa409b262c49cfc4f9
code-grey <adrishbora@outlook.com>
2025-03-01 12:55:05 +0530

    added support for environment variable access for db credentials

--- db/db.go
+++ db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
     "log"
+    "os"
 	"digi-notice-board/models"
     "gorm.io/driver/mysql"
     "gorm.io/gorm"
@@ -12,7 +13,20 @@ var DB *gorm.DB
 
 // Connect initializes the DB connection
 func Connect() {
-    dsn := "root:Rubidium@85@tcp(127.0.0.1:3306)/digital_notice_db?charset=utf8mb4&parseTime=True&loc=Local"
+
+	user := os.Getenv("MYSQL_USER")       // e.g. "root"
+    pass := os.Getenv("MYSQL_PASSWORD")   // e.g. "secretpassword"
+    host := os.Getenv("MYSQL_HOST")       // e.g. "127.0.0.1"
+    port := os.Getenv("MYSQL_PORT")       // e.g. "3306"
+    dbName := os.Getenv("MYSQL_DB")       // e.g. "digital_notice_db"
+    
+       if user == "" || pass == "" || host == "" || port == "" || dbName == "" {
+        log.Fatal("Missing one or more required environment variables")
+    }
+
+
+        dsn := user + ":" + pass + "@tcp(" + host + ":" + port + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
+
     var err error
 
 
